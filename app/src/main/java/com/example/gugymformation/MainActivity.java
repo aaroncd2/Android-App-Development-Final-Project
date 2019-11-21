a85ef5451a30d1ace1422e2baa7c2702d41163a2
Eric Av <eric av>
2019-11-20 20:39:32 -0800

    second commit

--- app/src/main/java/com/example/gugymformation/MainActivity.java
+++ app/src/main/java/com/example/gugymformation/MainActivity.java
@@ -4,7 +4,13 @@ package com.example.gugymformation;
 //By Eric Av and Aaron Dodge
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +18,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button hoursButton = (Button) findViewById(R.id.hoursButton);
+        hoursButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HoursActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button infoButton = (Button) findViewById(R.id.infoButton);
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button workoutButton = (Button) findViewById(R.id.workoutButton);
+        workoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WorkoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
